6e14ce9b72c7c3ea85da4a93ac07e93375e5c9cb
Bharat <bharatramesh2601@gmail.com>
2025-02-12 18:22:28 -0800

    updated the project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'bharatramesh.dev' title`, () => {
+  it(`should have the 'oneinterview' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bharatramesh.dev');
+    expect(app.title).toEqual('oneinterview');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bharatramesh.dev');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, oneinterview');
   });
 });
